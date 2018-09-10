061e241f6686d48f3fe5f6ac8b7f2c25ae035140
Cristiano Oliveira Lopes <39673976+lopesco@users.noreply.github.com>
2018-09-10 10:26:10 -0300

    Update Page2.js

--- src/containers/Page2.js
+++ src/containers/Page2.js
@@ -54,6 +54,49 @@ export default class Page2 extends Component {
             height="600px"
           />
         </div>
+        <center>
+          <div class="panel panel-default">
+            <div class="panel-heading">Selecione corretamente:</div>
+            <div class="panel-body">
+              <p>
+                Estrutura A:{" "}
+                <select class="custom-select">
+                  <option selected>Opções</option>
+                  <option value="1">Opção 1</option>
+                  <option value="2">Opção 2</option>
+                  <option value="3">Opção 3</option>
+                </select>
+              </p>
+              <p>
+                Estrutura B:{" "}
+                <select class="custom-select">
+                  <option selected>Opções</option>
+                  <option value="1">Opção 1</option>
+                  <option value="2">Opção 2</option>
+                  <option value="3">Opção 3</option>
+                </select>
+              </p>
+              <p>
+                Estrutura C:{" "}
+                <select class="custom-select">
+                  <option selected>Opções</option>
+                  <option value="1">Opção 1</option>
+                  <option value="2">Opção 2</option>
+                  <option value="3">Opção 3</option>
+                </select>
+              </p>
+              <p>
+                Estrutura D:{" "}
+                <select class="custom-select">
+                  <option selected>Opções</option>
+                  <option value="1">Opção 1</option>
+                  <option value="2">Opção 2</option>
+                  <option value="3">Opção 3</option>
+                </select>
+              </p>
+            </div>
+          </div>
+        </center>
         <button type="submit" class="btn btn-primary">
           Salvar
         </button>

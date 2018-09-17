13167419d8b5fb331499d97d6c90de5ed1b7ad5c
Cristiano Oliveira Lopes <39673976+lopesco@users.noreply.github.com>
2018-09-17 09:28:13 -0300

    Update Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -9,6 +9,9 @@ import Page1 from "./containers/Page1";
 import Page2 from "./containers/Page2";
 import Page3 from "./containers/Page3";
 import Page4 from "./containers/Page4";
+import Page5 from "./containers/Page5";
+import Page6 from "./containers/Page6";
+import Page7 from "./containers/Page7";
 
 export default () => (
   <Switch>
@@ -20,6 +23,9 @@ export default () => (
     <Route path="/page2" exact component={Page2} />
     <Route path="/page3" exact component={Page3} />
     <Route path="/page4" exact component={Page4} />
+    <Route path="/page5" exact component={Page5} />
+    <Route path="/page6" exact component={Page6} />
+    <Route path="/page7" exact component={Page7} />
     <Route component={NotFound} />
   </Switch>
 );
